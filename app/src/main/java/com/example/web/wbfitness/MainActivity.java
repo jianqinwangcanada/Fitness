089da4ccb0a6d6cd9fe99e899403638d919aa322
Jonathon Bauer <jonathon.bauer01@stclairconnect.ca>
2018-11-15 09:03:29 -0500

    Revert "#9 Initial commit Setting Functionality"

--- app/src/main/java/com/example/web/wbfitness/MainActivity.java
+++ app/src/main/java/com/example/web/wbfitness/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.web.wbfitness;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -12,7 +10,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.util.DisplayMetrics;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -22,8 +19,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
@@ -37,9 +32,7 @@ public class MainActivity extends AppCompatActivity
 
     // Declare the SharedPreferences File
     SharedPreferences preferences = null;
-    //Declare Locale variable
-    Locale myLocal;
-    SharedPreferences setting;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,14 +62,14 @@ public class MainActivity extends AppCompatActivity
 
 
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -161,25 +154,8 @@ public class MainActivity extends AppCompatActivity
         {
 
         }else if(id==R.id.nav_refresh){
-            // this method will refresh language the setting choice
-            //Populate the the setting SharedPreference
-            setting=PreferenceManager.getDefaultSharedPreferences(this);
-
-            //Grab the setting value user chosen
-            String lanugageChoice=setting.getString(getResources().getString(R.string.language_key),"");
-            System.out.println("God Luanguage"+lanugageChoice);
 
 
-            //If user chose Chinese
-            if(lanugageChoice.equals("0")){
-                myLocal=Locale.SIMPLIFIED_CHINESE;
-                  }
-                  //if user choose english
-            else if(lanugageChoice.equals("1")){
-                myLocal=Locale.ENGLISH;
-                   }
-           //Call method to refresh mainavtivity
-            setLocale(myLocal);
         }
 
 
@@ -195,25 +171,6 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onFragmentInteraction(Uri uri) {
-
-    }
-    // Change the language
-    public void setLocale(Locale myLocal) {
-        //Declare the resource
-        Resources res = getResources();
-        //Declare DisplayMetrics using resources
-        DisplayMetrics dm = res.getDisplayMetrics();
-        //Declare the conf
-        Configuration conf = res.getConfiguration();
-        //set the Configuration with myLocal
-        conf.locale = myLocal;
-        //Update configuration with resource
-        res.updateConfiguration(conf, dm);
-        //Declare the intent
-
-        Intent refresh = new Intent(this, MainActivity.class);
-        //Refresh the mainActivity
-        startActivity(refresh);
 
     }
 }
